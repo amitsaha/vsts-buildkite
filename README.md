0c379f74ace556ee0096fceed3fabbb250790829
Amit Saha <amitsaha.in@gmail.com>
2018-07-23 16:51:51 +1000

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# Visual Studio Team Services + BuildKite
+
+Required environment variables:
+
+- `BUILDKITE_URL`: Buildkite pipeline URL for creating a build (Example: "https://api.buildkite.com/v2/organizations/<org>/pipelines/<pipeline>/builds")
+- `BUILDKITE_AUTH_TOKEN`: API Token with permissions to create a build (Example: "asasa")
---
+++ main.go
@@ -0,0 +1,294 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+// https://mholt.github.io/json-to-go/ is awesome
+type VSTSPayload struct {
+	SubscriptionID string `json:"subscriptionId"`
+	NotificationID int    `json:"notificationId"`
+	ID             string `json:"id"`
+	EventType      string `json:"eventType"`
+	PublisherID    string `json:"publisherId"`
+	Message        struct {
+		Text     string `json:"text"`
+		HTML     string `json:"html"`
+		Markdown string `json:"markdown"`
+	} `json:"message"`
+	DetailedMessage struct {
+		Text     string `json:"text"`
+		HTML     string `json:"html"`
+		Markdown string `json:"markdown"`
+	} `json:"detailedMessage"`
+	Resource struct {
+		Commits []struct {
+			CommitID string `json:"commitId"`
+			Author   struct {
+				Name  string    `json:"name"`
+				Email string    `json:"email"`
+				Date  time.Time `json:"date"`
+			} `json:"author"`
+			Committer struct {
+				Name  string    `json:"name"`
+				Email string    `json:"email"`
+				Date  time.Time `json:"date"`
+			} `json:"committer"`
+			Comment string `json:"comment"`
+			URL     string `json:"url"`
+		} `json:"commits"`
+		RefUpdates []struct {
+			Name        string `json:"name"`
+			OldObjectID string `json:"oldObjectId"`
+			NewObjectID string `json:"newObjectId"`
+		} `json:"refUpdates"`
+		Repository struct {
+			ID      string `json:"id"`
+			Name    string `json:"name"`
+			URL     string `json:"url"`
+			Project struct {
+				ID         string `json:"id"`
+				Name       string `json:"name"`
+				URL        string `json:"url"`
+				State      string `json:"state"`
+				Visibility string `json:"visibility"`
+			} `json:"project"`
+			DefaultBranch string `json:"defaultBranch"`
+			RemoteURL     string `json:"remoteUrl"`
+		} `json:"repository"`
+		PushedBy struct {
+			DisplayName string `json:"displayName"`
+			ID          string `json:"id"`
+			UniqueName  string `json:"uniqueName"`
+		} `json:"pushedBy"`
+		PushID int       `json:"pushId"`
+		Date   time.Time `json:"date"`
+		URL    string    `json:"url"`
+	} `json:"resource"`
+	ResourceVersion    string `json:"resourceVersion"`
+	ResourceContainers struct {
+		Collection struct {
+			ID string `json:"id"`
+		} `json:"collection"`
+		Account struct {
+			ID string `json:"id"`
+		} `json:"account"`
+		Project struct {
+			ID string `json:"id"`
+		} `json:"project"`
+	} `json:"resourceContainers"`
+	CreatedDate time.Time `json:"createdDate"`
+}
+
+type BuildkitePayload struct {
+	Commit  string `json:"commit"`
+	Branch  string `json:"branch"`
+	Message string `json:"message"`
+	Author  struct {
+		Name  string `json:"name"`
+		Email string `json:"email"`
+	} `json:"author"`
+	Env struct {
+		MYENVVAR string `json:"MY_ENV_VAR"`
+	} `json:"env"`
+	MetaData struct {
+		SomeBuildData  string `json:"some build data"`
+		OtherBuildData bool   `json:"other build data"`
+	} `json:"meta_data"`
+}
+
+type BuildkiteCreateBuildResponse struct {
+	ID      string      `json:"id"`
+	URL     string      `json:"url"`
+	WebURL  string      `json:"web_url"`
+	Number  int         `json:"number"`
+	State   string      `json:"state"`
+	Blocked bool        `json:"blocked"`
+	Message string      `json:"message"`
+	Commit  string      `json:"commit"`
+	Branch  string      `json:"branch"`
+	Tag     interface{} `json:"tag"`
+	Env     struct {
+		MYENVVAR string `json:"MY_ENV_VAR"`
+	} `json:"env"`
+	Source  string `json:"source"`
+	Creator struct {
+		ID        string    `json:"id"`
+		Name      string    `json:"name"`
+		Email     string    `json:"email"`
+		AvatarURL string    `json:"avatar_url"`
+		CreatedAt time.Time `json:"created_at"`
+	} `json:"creator"`
+	CreatedAt   time.Time   `json:"created_at"`
+	ScheduledAt time.Time   `json:"scheduled_at"`
+	StartedAt   interface{} `json:"started_at"`
+	FinishedAt  interface{} `json:"finished_at"`
+	MetaData    struct {
+		SomeBuildData  string `json:"some build data"`
+		OtherBuildData string `json:"other build data"`
+	} `json:"meta_data"`
+	PullRequest interface{} `json:"pull_request"`
+	Pipeline    struct {
+		ID                              string      `json:"id"`
+		URL                             string      `json:"url"`
+		WebURL                          string      `json:"web_url"`
+		Name                            string      `json:"name"`
+		Description                     string      `json:"description"`
+		Slug                            string      `json:"slug"`
+		Repository                      string      `json:"repository"`
+		BranchConfiguration             interface{} `json:"branch_configuration"`
+		DefaultBranch                   string      `json:"default_branch"`
+		SkipQueuedBranchBuilds          bool        `json:"skip_queued_branch_builds"`
+		SkipQueuedBranchBuildsFilter    interface{} `json:"skip_queued_branch_builds_filter"`
+		CancelRunningBranchBuilds       bool        `json:"cancel_running_branch_builds"`
+		CancelRunningBranchBuildsFilter interface{} `json:"cancel_running_branch_builds_filter"`
+		Provider                        struct {
+			ID       string `json:"id"`
+			Settings struct {
+			} `json:"settings"`
+		} `json:"provider"`
+		BuildsURL string    `json:"builds_url"`
+		BadgeURL  string    `json:"badge_url"`
+		CreatedAt time.Time `json:"created_at"`
+		Env       struct {
+		} `json:"env"`
+		ScheduledBuildsCount int `json:"scheduled_builds_count"`
+		RunningBuildsCount   int `json:"running_builds_count"`
+		ScheduledJobsCount   int `json:"scheduled_jobs_count"`
+		RunningJobsCount     int `json:"running_jobs_count"`
+		WaitingJobsCount     int `json:"waiting_jobs_count"`
+		Steps                []struct {
+			Type                string `json:"type"`
+			Name                string `json:"name"`
+			Command             string `json:"command"`
+			ArtifactPaths       string `json:"artifact_paths"`
+			BranchConfiguration string `json:"branch_configuration"`
+			Env                 struct {
+			} `json:"env"`
+			TimeoutInMinutes interface{} `json:"timeout_in_minutes"`
+			AgentQueryRules  []string    `json:"agent_query_rules"`
+			Concurrency      interface{} `json:"concurrency"`
+			Parallelism      interface{} `json:"parallelism"`
+		} `json:"steps"`
+	} `json:"pipeline"`
+	Jobs []struct {
+		ID              string      `json:"id"`
+		Type            string      `json:"type"`
+		Name            string      `json:"name"`
+		AgentQueryRules []string    `json:"agent_query_rules"`
+		State           string      `json:"state"`
+		BuildURL        string      `json:"build_url"`
+		WebURL          string      `json:"web_url"`
+		LogURL          string      `json:"log_url"`
+		RawLogURL       string      `json:"raw_log_url"`
+		ArtifactsURL    string      `json:"artifacts_url"`
+		Command         string      `json:"command"`
+		ExitStatus      interface{} `json:"exit_status"`
+		ArtifactPaths   string      `json:"artifact_paths"`
+		Agent           interface{} `json:"agent"`
+		CreatedAt       time.Time   `json:"created_at"`
+		ScheduledAt     time.Time   `json:"scheduled_at"`
+		StartedAt       interface{} `json:"started_at"`
+		FinishedAt      interface{} `json:"finished_at"`
+		Retried         bool        `json:"retried"`
+		RetriedInJobID  interface{} `json:"retried_in_job_id"`
+		RetriesCount    interface{} `json:"retries_count"`
+	} `json:"jobs"`
+}
+
+func vstsHandler(resp http.ResponseWriter, req *http.Request) {
+
+	if req.Method != "POST" {
+		http.Error(resp, "This endpoint doesn't support this request type", http.StatusMethodNotAllowed)
+		return
+	}
+
+	reqBody, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	payload := VSTSPayload{}
+	err = json.Unmarshal([]byte(reqBody), &payload)
+	if err != nil {
+		panic(err)
+	}
+
+	if payload.EventType != "git.push" {
+		log.Printf("Unsupported event type: %s\n", payload.EventType)
+		http.Error(resp, "Event not supported: "+payload.EventType, http.StatusBadRequest)
+		return
+	}
+
+	if len(payload.Resource.Commits) == 0 {
+		log.Printf("Payload with no commits: %#v\n", string(reqBody))
+		http.Error(resp, "No commits data found in request", http.StatusBadRequest)
+		return
+	}
+
+	buildkiteURL := os.Getenv("BUILDKITE_URL")
+	if buildkiteURL == "" {
+		log.Fatal("Please specify BUILDKITE_URL")
+	}
+
+	buildkiteAuthToken := os.Getenv("BUILDKITE_AUTH_TOKEN")
+	if buildkiteAuthToken == "" {
+		log.Fatal("Please specify BUILDKITE_AUTH_TOKEN")
+	}
+
+	buildkitePayload := BuildkitePayload{}
+	buildkitePayload.Commit = payload.Resource.Commits[0].CommitID
+
+	refsUpdates := payload.Resource.RefUpdates[0].Name
+	// Deriving branch from refsUpdates
+	buildkitePayload.Branch = strings.Split(refsUpdates, "/")[2]
+
+	buildkitePayload.Author.Name = payload.Resource.Commits[0].Author.Name
+	buildkitePayload.Author.Email = payload.Resource.Commits[0].Author.Email
+	buildkitePayload.Message = payload.Resource.Commits[0].Comment
+
+	buildkitePayloadString, err := json.Marshal(buildkitePayload)
+
+	if err != nil {
+		log.Println("Couldn't create buildkite post body")
+	}
+
+	log.Printf("Firing off a build to buildkite: %s %s %s\n", buildkitePayload.Author.Name, buildkitePayload.Branch, buildkitePayload.Commit)
+
+	buildkiteReq, err := http.NewRequest("POST", buildkiteURL, bytes.NewBuffer(buildkitePayloadString))
+
+	buildkiteReq.Header.Set("Authorization", "Bearer "+buildkiteAuthToken)
+	buildkiteReq.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	buildkiteResp, err := client.Do(buildkiteReq)
+	if err != nil {
+		panic(err)
+	}
+	defer buildkiteResp.Body.Close()
+	body, err := ioutil.ReadAll(buildkiteResp.Body)
+	if err != nil {
+		panic(err)
+	}
+	response := BuildkiteCreateBuildResponse{}
+	err = json.Unmarshal([]byte(body), &response)
+	fmt.Println("Build created: ", response.Jobs[0].WebURL)
+
+}
+
+func setupServer() {
+	http.HandleFunc("/", vstsHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func main() {
+	setupServer()
+}
